0c3d6c2454de13a7806eb89cff2cb44e77c3767a
rxMATTEO <erik_sundberg@mail.ru>
2023-12-17 19:43:51 +0300

    Table done

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,44 @@
 <script setup lang="ts">
 import axios from "axios";
-import {onMounted, ref} from "vue";
+import {computed, onMounted, Ref, ref} from "vue";
+
+type PersonInfo = {
+  event: string[]
+}
+
+type Person = {
+  firstName: string
+  lastName: string
+  middleName: string
+  page: string
+  pageNumber: string
+  room: string
+  rows: string[]
+  info: PersonInfo[]
+}
 
 const body = ref(null);
 const info = ref(null);
-const people = ref(null);
+const people: Ref<Person[] | null> = ref(null);
+
 onMounted(async () => {
   body.value = (await axios.get('http://localhost:3000/')).data;
   info.value = (await axios.get('http://localhost:3000/info')).data;
   people.value = (await axios.get('http://localhost:3000/people')).data;
-  console.log(people.value)
-})
+});
+
+const columns = computed(() => {
+  return {
+    firstName: 'Имя',
+    lastName: 'Фамилия',
+    middleName: 'Отчество',
+    page: 'Страница',
+    pageNumber: 'Номер страницы',
+    room: 'Комната',
+    rows: 'Строки',
+    info: 'Информация (входы-выходы)',
+  };
+});
 </script>
 
 <template>
@@ -28,13 +56,13 @@ onMounted(async () => {
       <div>
         Время: {{ info.time }}
       </div>
-      {{
-      people
-      }}
-      <!--  <DataTable :value="headers?.data">-->
-      <!--    <Column v-for="header in headers?.data" :header="header" />-->
-      <!--  </DataTable>-->
     </div>
+
+    <template v-if="people">
+      <DataTable :value="people">
+        <Column v-for="[k,v] in Object.entries(columns)" :header="v" :field="k" />
+      </DataTable>
+    </template>
   </div>
 </template>
 
