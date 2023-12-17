78a439971e72b51e038bb3802d98994db4bab4ed
rxMATTEO <erik_sundberg@mail.ru>
2023-12-17 15:00:27 +0300

    Change server, instll vue

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 const mammoth = require("mammoth");
 const fs = require('fs');
-var express = require('express');
-var app = express();
+const DOMParser = require('universal-dom-parser');
+const express = require('express');
+const cors = require("cors");
+const app = express();
+const jsdom = require("jsdom");
 
+app.use(cors());
+function parse(htmlString){
+  const parser = new jsdom.JSDOM(htmlString);
+  console.log(parser.window.document.querySelectorAll('p')[0]);
+}
 app.get('/', async function(req, res) {
   const result = await mammoth.convertToHtml({ path: 'ri.docx' });
-  res.send(result.value)
+  parse(result.value);
+  res.send(result.value);
 });
 console.log('listening on port 3000');
 app.listen(3000);
+
---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import axios from "axios";
+import {onMounted, ref} from "vue";
+
+let result = ref(null);
+onMounted(async () => {
+  result.value = await axios.get('http://localhost:3000/');
+})
+</script>
+
+<template>
+  <div v-html="result?.data">
+  </div>
+</template>
+
+<style scoped>
+</style>
