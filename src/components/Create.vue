1a7f430c8d1d66026175d062efed973d9de1cf40
rxMATTEO <erik_sundberg@mail.ru>
2024-01-11 10:22:00 +0300

    Move rtf upload to component and stuff

--- src/components/Create.vue
+++ src/components/Create.vue
@@ -1,53 +1,6 @@
 <script setup lang="ts">
-import {useToast} from "primevue/usetoast";
-import {computed, inject, ref} from "vue";
-import {usePrimeVue} from "primevue/config";
-import axios from "axios";
-
-const toast = useToast();
-const api = inject('apiUrl');
-
-const totalSize = ref(0);
-const totalSizePercent = ref(0);
-const files = ref([]);
-const {config: {locale: {fileSizeTypes}}} = usePrimeVue();
-
-const onRemoveTemplatingFile = (file, removeFileCallback, index) => {
-  removeFileCallback(index);
-  totalSize.value -= parseInt(formatSize(file.size));
-  totalSizePercent.value = totalSize.value / 10;
-};
-
-const onSelectedFiles = (event) => {
-  files.value = event.files;
-  files.value.forEach((file) => {
-    totalSize.value += parseInt(formatSize(file.size));
-  });
-};
-
-const uploadEvent = (callback) => {
-  totalSizePercent.value = totalSize.value / 10;
-  callback();
-};
-
-const onTemplatedUpload = () => {
-  toast.add({severity: "info", summary: "Ура!", detail: "Файл загружен", life: 3000});
-  stepIndex.value += 1;
-};
-const formatSize = (bytes) => {
-  const k = 1024;
-  const dm = 3;
-  const sizes = fileSizeTypes;
-
-  if (bytes === 0) {
-    return `0 ${sizes[0]}`;
-  }
-
-  const i = Math.floor(Math.log(bytes) / Math.log(k));
-  const formattedSize = parseFloat((bytes / Math.pow(k, i)).toFixed(dm));
-
-  return `${formattedSize} ${sizes[i]}`;
-};
+import {computed, ref} from "vue";
+import RtfUpload from "./RtfUpload.vue";
 
 const steps = computed(() => {
   return [
@@ -64,6 +17,14 @@ const steps = computed(() => {
 });
 
 const stepIndex = ref(0);
+
+const currentComponent = computed(() => {
+  switch (stepIndex.value) {
+    case 0: {
+      return RtfUpload;
+    }
+  }
+});
 </script>
 
 <template>
@@ -71,66 +32,6 @@ const stepIndex = ref(0);
   <div class="card w-8 m-auto">
     <Steps :model="steps" readonly v-model:activeStep="stepIndex" />
     <Toast/>
-    <FileUpload name="rtf" :url="`${api}/create`" @upload="onTemplatedUpload($event)" :multiple="false" accept=".rtf"
-                :maxFileSize="1000000" @select="onSelectedFiles" :auto="true"
-                invalidFileTypeMessage="{0} - не rtf документ">
-      <template #header="{ chooseCallback, uploadCallback, clearCallback, files }">
-        <div class="flex flex-wrap justify-content-between align-items-center flex-1 gap-2">
-          <div class="flex gap-2">
-            <Button @click="chooseCallback()" icon="pi pi-file" rounded outlined></Button>
-            <Button @click="uploadEvent(uploadCallback)" icon="pi pi-cloud-upload" rounded outlined severity="success"
-                    :disabled="!files || files.length === 0"></Button>
-            <Button @click="clearCallback()" icon="pi pi-times" rounded outlined severity="danger"
-                    :disabled="!files || files.length === 0"></Button>
-          </div>
-          <ProgressBar :value="totalSizePercent" :showValue="false"
-                       :class="['md:w-20rem h-1rem w-full md:ml-auto', { 'exceeded-progress-bar': totalSizePercent > 100 }]">
-            <span class="white-space-nowrap">{{ totalSize }}B / 1Mb</span>
-          </ProgressBar>
-        </div>
-      </template>
-      <template #content="{ files, uploadedFiles, removeUploadedFileCallback, removeFileCallback }">
-        <div v-if="files.length > 0">
-          <h5>Pending</h5>
-          <div class="flex flex-wrap p-0 sm:p-5 gap-5">
-            <div v-for="(file, index) of files" :key="file.name + file.type + file.size"
-                 class="card m-0 px-6 flex flex-column border-1 surface-border align-items-center gap-3">
-              <div>
-                <img role="presentation" :alt="file.name" :src="file.objectURL" width="100" height="50"
-                     class="shadow-2"/>
-              </div>
-              <span class="font-semibold">{{ file.name }}</span>
-              <div>{{ formatSize(file.size) }}</div>
-              <Badge value="Pending" severity="warning"/>
-              <Button icon="pi pi-times" @click="onRemoveTemplatingFile(file, removeFileCallback, index)" outlined
-                      rounded severity="danger"/>
-            </div>
-          </div>
-        </div>
-
-        <div v-if="uploadedFiles.length > 0">
-          <h5>Успешно загружены:</h5>
-          <div class="flex flex-wrap p-0 sm:p-5 gap-5">
-            <div v-for="(file, index) of uploadedFiles" :key="file.name + file.type + file.size"
-                 class="card m-0 px-6 py-2 flex flex-column border-1 surface-border align-items-center gap-3">
-              <div>
-                <img role="presentation" :alt="file.name" src="/rtf.png" width="150" height="150"
-                     class="shadow-2"/>
-              </div>
-              <span class="font-semibold">{{ file.name }}</span>
-              <div>{{ formatSize(file.size) }}</div>
-              <Badge value="Загружен" class="mt-3" severity="success"/>
-              <Button icon="pi pi-times" @click="removeUploadedFileCallback(index)" outlined rounded severity="danger"/>
-            </div>
-          </div>
-        </div>
-      </template>
-      <template #empty>
-        <div class="flex align-items-center justify-content-center flex-column">
-          <i class="pi pi-cloud-upload border-2 border-circle p-5 text-8xl text-400 border-400"/>
-          <p class="mt-4 mb-0">Брось сюда rtf документ для его загрузки или нажми кнопку сверху.</p>
-        </div>
-      </template>
-    </FileUpload>
+    <component :is="currentComponent" v-model:step-index="stepIndex" />
   </div>
 </template>
