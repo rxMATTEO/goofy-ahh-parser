da72197e899ebf3a79a6e379e697781db454eec6
rxMATTEO <erik_sundberg@mail.ru>
2024-01-28 19:39:14 +0300

    Fix typo

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import {provide} from "vue";
 
-provide('apiUrl', import.meta.env.PROD ? 'http://localhost:3001/api' : 'https://pass.ri-advance.ru/api');
+provide('apiUrl', import.meta.env.PROD ? 'https://pass.ri-advance.ru/api' : 'http://localhost:3001/api');
 </script>
 
 <template>
