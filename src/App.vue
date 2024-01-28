126009f36c613f2870d090abf4182fb870f142ab
rxMATTEO <erik_sundberg@mail.ru>
2024-01-28 19:35:05 +0300

    Fix

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import {provide} from "vue";
 
-provide('apiUrl', import.meta.env.PROD ? 'http://localhost:3001/api' : 'http://localhost:3001/api');
+provide('apiUrl', import.meta.env.PROD ? 'http://localhost:3001/api' : 'https://pass.ri-advance.ru/api');
 </script>
 
 <template>
